5b00509dfff45e4927473df7895b3dcc41bf00db
warrenau <onsenkame@gmail.com>
2023-03-21 19:38:50 -0700

    removed buttons

--- pages/ongoing.jsx
+++ pages/ongoing.jsx
@@ -31,20 +31,6 @@ const twitch = () => {
             I believe that the generative abilities of GPT-4 can be used to fill holes in the accessibility of websites and allow for more users
             to be able to intuitively navigate through websites.
           </p>
-          <a
-            href='https://twitch-nextjs-tailwind.vercel.app/'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
-          </a>
-          <a
-            href='https://github.com/onsenkame/twitch-nextjs-tailwind'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4'>Code</button>
-          </a>
 
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
